57d21741d64abbce225b136a070ddc8149028363
Aaron J. Brown <39014977+aaronjosephbrown@users.noreply.github.com>
2023-05-06 20:38:57 -0500

    Updated Styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <Banner />
-      <div className='mx-auto max-w-8xl px-4 sm:px-6 lg:px-8'>
+      <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
         <NavBar />
         <Main />
       </div>
